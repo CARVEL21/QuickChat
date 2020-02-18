351a8cea10f957cc5758c385c70c5d569c6ea163
carvel <carvel@github.com>
2020-02-18 11:49:26 -0600

    First Commit

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by carlos on 18/02/20.
+//  Copyright © 2020 Carlos Velasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
