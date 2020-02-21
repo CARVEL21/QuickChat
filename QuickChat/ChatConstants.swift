9d5f748ca5d5e36719c6c7a3a7c970dce4064407
carvel <carvel@github.com>
2020-02-21 13:54:18 -0600

    Chat constants file

---
+++ QuickChat/ChatConstants.swift
@@ -0,0 +1,22 @@
+//
+//  ChatConstants.swift
+//  QuickChat
+//
+//  Created by carlos on 21/02/20.
+//  Copyright © 2020 Carlos Velasco. All rights reserved.
+//
+
+struct Constants {
+    static let appName = "📨QUICKCHAT"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
--- QuickChat/Controllers/ChatViewController.swift
+++ QuickChat/Controllers/ChatViewController.swift
@@ -17,7 +17,7 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "📨QUICKCHAT"
+        title = Constants.appName
         navigationItem.hidesBackButton = true
 
     }
