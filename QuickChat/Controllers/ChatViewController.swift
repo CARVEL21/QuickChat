145d0b2cd78007826f2ef5b715612a986b55e8fa
carvel <carvel@github.com>
2020-02-21 19:51:15 -0600

    MessageCell XIB

--- QuickChat/Controllers/ChatViewController.swift
+++ QuickChat/Controllers/ChatViewController.swift
@@ -14,11 +14,19 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "carlos@me.com", body: "hola, como estas"),
+        Message(sender: "Shirley@me.com", body: "hola, muy bien")
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = Constants.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
 
     }
 
@@ -33,7 +41,20 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print ("Error signing out: %@", signOutError)
         }
-          
-          
     }
 }
+// extend nuestro chat view controller para que pueda adoptar el protocolo UITableViewDataSource quiere decir que cuando nuestro table view cargue va a hacer una solicitud de data
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+            as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+
---
+++ QuickChat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  QuickChat
+//
+//  Created by carlos on 21/02/20.
+//  Copyright © 2020 Carlos Velasco. All rights reserved.
+//
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
